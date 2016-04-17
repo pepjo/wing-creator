7226a75bd57760fb6698814578628fd72286afd2
Pep Rodeja <pepjo26@gmail.com>
2016-04-18 01:40:45 +0200

    add download dialog

--- src/components/downloadBtn/DownloadBtn.jsx
+++ src/components/downloadBtn/DownloadBtn.jsx
@@ -7,13 +7,12 @@ import saveAs from 'browser-saveas'
 // Functions
 import GIDobject from './GIDfunctions/GIDobject'
 
-// TODO: DELETE THIS
-import * as data from './GIDfunctions/__test__/GIDobject.data'
-
 // Components
 import IconButton from 'material-ui/IconButton'
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 import DownloadIcon from 'material-ui/svg-icons/file/file-download'
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
 
 // Styles
 import * as style from './DownloadBtn.style.js'
@@ -43,9 +42,16 @@ class DownloadBtn extends React.Component {
     super(props)
 
     this.downloadGid = this.downloadGid.bind(this)
+    this.handleCloseDialog = this.handleCloseDialog.bind(this)
+
+    this.state = {
+      open: false,
+    }
   }
 
   downloadGid () {
+    this.setState({ open: true })
+
     const zip = new JSZip()
 
     const vertices = this.props.airfoilShell.vertices.map((airfoil) => (
@@ -74,22 +80,63 @@ class DownloadBtn extends React.Component {
     })
   }
 
+  handleCloseDialog () {
+    this.setState({ open: false })
+  }
+
   render () {
+    let btn
+
     if (this.props.circle) {
-      return (
+      btn = (
         <FloatingActionButton onClick={this.downloadGid}>
           <DownloadIcon />
         </FloatingActionButton>
       )
+    } else {
+      btn = (
+        <IconButton
+          style={style.barIcon} tooltip="Download GID .geo"
+          touch tooltipPosition="bottom-left"
+          onClick={this.downloadGid}
+        >
+          <DownloadIcon color="white" />
+        </IconButton>
+      )
     }
+
+    const actions = [
+      <FlatButton
+        label="Ok"
+        primary
+        keyboardFocused
+        onTouchTap={this.handleCloseDialog}
+      />,
+    ]
+
     return (
-      <IconButton
-        style={style.barIcon} tooltip="Download GID .geo"
-        touch tooltipPosition="bottom-left"
-        onClick={this.downloadGid}
-      >
-        <DownloadIcon color="white" />
-      </IconButton>
+      <span>
+        {btn}
+        <Dialog
+          title="Downloading..."
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          If you have an airfoil selected the download should start any moment.<br /><br />
+          You will need GID 12 or newer; grab your free copy here:
+          <FlatButton style={{ position: 'relative', top: -2 }}
+            primary
+            label="Download GID"
+            onClick={() => {
+              window.open('http://www.gidhome.com/download')
+            }}
+          />
+         <br /><br />
+          If you have problems downloading try using the last version of Chrome.
+        </Dialog>
+      </span>
     )
   }
 }
